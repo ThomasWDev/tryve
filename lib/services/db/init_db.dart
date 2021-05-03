dc10ec3cda8507aca9c596fce30fec94263e2514
qjkxbmwvz <stephen@wtberks.com>
2021-05-03 13:20:35 -0800

    Initial web version.

--- lib/services/db/init_db.dart
+++ lib/services/db/init_db.dart
@@ -1,10 +1,10 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart' as path;
 
-void initHive(VoidCallback callback) {
-  path.getApplicationDocumentsDirectory().then((appDocumentDir) {
-    Hive.init(appDocumentDir.path);
+void initHive(VoidCallback callback) async {
+  if (kIsWeb) {
     Hive.openBox('settings').then((_) {
       Hive.openBox('data').then((_) {
         Hive.openBox('user').then((_) {
@@ -12,5 +12,16 @@ void initHive(VoidCallback callback) {
         });
       });
     });
-  });
+  } else {
+    path.getApplicationDocumentsDirectory().then((appDocumentDir) {
+      Hive.init(appDocumentDir.path);
+      Hive.openBox('settings').then((_) {
+        Hive.openBox('data').then((_) {
+          Hive.openBox('user').then((_) {
+            callback();
+          });
+        });
+      });
+    });
+  }
 }
